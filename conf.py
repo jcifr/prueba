5a7ef4e65b02e8b491698d4bdc46b86d2efbd412
jaime <jcifr@unileon.es>
2018-10-08 16:46:07 +0200

    lunes8

--- conf.py
+++ conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'exam'
-copyright = u'2018, mi'
+copyright = u'2018, mito'
 author = u'mi'
 
 # The version info for the project you're documenting, acts as replacement for
